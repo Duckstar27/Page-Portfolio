7457b2709ded0473be2271630d7855aae6e881b1
Duckstar27 <manhducngul@gmail.com>
2024-11-02 05:20:18 +0700

    2

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-const slider = document.getElementById('slider');
-
-    function updateSliderBackground() {
-        const value = slider.value;
-        slider.style.background = `linear-gradient(to right, orange ${value}%, #ddd ${value}%)`;
-    }
-
-slider.addEventListener('input', updateSliderBackground);
-updateSliderBackground();
